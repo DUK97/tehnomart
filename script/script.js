efcaf810dffc449b1f593b6f15599298188333e0
DUK97 <mrduk97@gmail.com>
2021-05-19 17:43:38 +0700

    slider almost fixed, items and manufacturers done

--- script/script.js
+++ script/script.js
@@ -2,26 +2,25 @@ const slides = document.querySelectorAll(".slider-slide");
 const btnNext = document.querySelector(".slider-btn-next");
 const btnPrev = document.querySelector(".slider-btn-prev");
 const slider = document.querySelector(".slider");
+let i = 0;
 
 slider.addEventListener("click", (e) => {
-  let i = 0;
-  debugger;
   if (e.target === btnNext) {
-    slides[i].classList.toggle("slider-slide-current");
+    slides[i].classList.remove("slider-slide-current");
     if (slides[i + 1] === undefined) {
-      slides[0].classList.toggle("slider-slide-current");
+      slides[0].classList.add("slider-slide-current");
       i = 0;
     } else {
-      slides[i + 1].classList.toggle("slider-slide-current");
+      slides[i + 1].classList.add("slider-slide-current");
       i++;
     }
   } else if (e.target === btnPrev) {
-    slides[i].classList.toggle("slider-slide-current");
+    slides[i].classList.remove("slider-slide-current");
     if (slides[i - 1] === undefined) {
-      slides[slides.length - 1].classList.toggle("slider-slide-current");
+      slides[slides.length - 1].classList.add("slider-slide-current");
       i = slides.length - 1;
     } else {
-      slides[i - 1].classList.toggle("slider-slide-current");
+      slides[i - 1].classList.add("slider-slide-current");
       i--;
     }
   }
